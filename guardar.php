ad107bb73875d778180ff6fe117ea1e1f4b54fa1
Lostrodzianko <jhonyalexanderc30@gmail.com>
2024-10-06 03:40:00 -0500

    Mi primer commit que emocion jasjajas

---
+++ guardar.php
@@ -0,0 +1,30 @@
+<?php
+// Conexión a la base de datos
+$servername = "tu_servidor";
+$username = "tu_usuario";
+$password = "tu_contraseña";
+$dbname = "tu_base_de_datos";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Obtener datos del formulario
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+
+// Insertar datos en la base de datos
+$sql = "INSERT INTO usuarios (nombre, correo) VALUES ('$nombre', '$correo')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Nuevo registro creado correctamente";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
